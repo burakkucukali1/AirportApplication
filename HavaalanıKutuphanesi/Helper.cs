67b9bcfcd8bfd7e478e5c1263872057fdf116280
Burak <51200138+burakkucukali1@users.noreply.github.com>
2019-05-30 20:01:45 +0300

    Initial commit

---
+++ HavaalanıKutuphanesi/Helper.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HavaalanıKutuphanesi
+{
+	public class Helper
+	{
+		public static char KlavyedenKarakterOku()
+		{
+			ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+			return keyInfo.KeyChar;
+		}
+		public static void EkraniTemizle()
+		{
+			Console.Clear();
+		}
+		public static void Bekle()
+		{
+			Console.ReadKey();
+		}
+		public static void EkranaYaz(int sayi)
+		{
+			EkranaYaz(sayi.ToString());
+		}
+
+		public static void EkranaYaz(string metin)
+		{
+			Console.WriteLine(metin);
+		}
+		public static void EkranaYaz(int[] sayilar)
+		{
+			for (int i = 0; i < sayilar.Length; i++)
+			{
+				EkranaYaz(sayilar[i] + " ");
+			}
+		}
+		public static void SatirBasiYap()
+		{
+			Console.WriteLine();
+		}
+		public static string EkrandanOku()
+		{
+			return Console.ReadLine();
+		}
+		public static void DiziyeElemanEkle(ref string[,] dizi, string birinciBoyutDegeri, string ikinciBoyutDegeri)
+		{
+			string[,] yeniDizi = new string[dizi.GetLength(0) + 1, dizi.GetLength(1)];
+			DiziKopyala(dizi, yeniDizi);
+
+			yeniDizi[dizi.GetLength(0), 0] = birinciBoyutDegeri;
+			yeniDizi[dizi.GetLength(0), 1] = ikinciBoyutDegeri;
+
+			dizi = yeniDizi;
+		}
+		public static void DiziKopyala(string[,] kaynakDizi, string[,] hedefDizi)
+		{
+			for (int i = 0; i < kaynakDizi.GetLength(0); i++)
+			{
+				for (int j = 0; j < kaynakDizi.GetLength(1); j++)
+				{
+					hedefDizi[i, j] = kaynakDizi[i, j];
+				}
+			}
+		}
+		public static int KontrolluSayiAl(string istekMesaji, string hataMesaji)
+		{
+			EkranaYaz(istekMesaji);
+			string input = EkrandanOku();
+			int sayi;
+			bool donustuMu = int.TryParse(input, out sayi);
+			if (donustuMu)
+			{
+				return sayi;
+			}
+			EkranaYaz(hataMesaji);
+			SatirBasiYap();
+			return KontrolluSayiAl(istekMesaji, hataMesaji);
+		}
+	}
+}
---
+++ HavaalanıUygulaması/Program.cs
@@ -0,0 +1,127 @@
+﻿using HavaalanıKutuphanesi;
+using System;
+
+namespace HavaalanıUygulaması
+{
+	class Program
+	{
+		const int koltukSayisi = 20;
+		const int businessClassKoltukSayisi = 8;
+		const int economyClassKoltukSayisi = 12;
+		static string[] koltuklar = new string[koltukSayisi];
+	
+
+		static void Main(string[] args)
+		{
+			KarsilamaEkrani();
+			Helper.Bekle();
+			Helper.EkraniTemizle();
+			for (; ; )
+			{
+				SınıfSecimi();
+				Helper.Bekle();
+				Helper.EkraniTemizle();
+			}
+		}
+		static void KarsilamaEkrani()
+		{
+			Helper.EkranaYaz("*************************************************");
+			Helper.EkranaYaz("Havaalanı Rezervasyon Uygulamasına Hoşgeldiniz.");
+			Helper.EkranaYaz("************************************************");
+
+		}
+
+		static void SınıfSecimi()
+		{
+			string secim = "";
+			Helper.EkranaYaz(" 1. Business Class bölümü için 1 tuşuna basın.\n 2. Economy Class bölümü için 2 tuşuna basın.");
+		
+			do
+			{
+				secim = Console.ReadKey(true).KeyChar.ToString();
+
+			} while (secim != "1" && secim != "2");
+			if (secim == "1")
+			{
+				Helper.EkraniTemizle();
+				Helper.EkranaYaz("Business Class bölümünde kalan boş koltuklar: ");
+
+				for (int i = 0; i <8; i++)
+				{
+					Helper.EkranaYaz((1+i)+ "-" +koltuklar[i]);
+				}
+			}
+			else
+			{
+				Helper.EkraniTemizle();
+				Helper.EkranaYaz("Economy Class bölümünde kalan boş koltuklar: ");
+
+				for (int i = 8; i < 20; i++)
+				{
+					Helper.EkranaYaz((1+i)+ "-"+ koltuklar[i]);
+				}
+			}
+			int kullanıcınınSectigiKoltuk = Helper.KontrolluSayiAl("","");
+
+			//Business
+			if (secim=="1" && kullanıcınınSectigiKoltuk<9 && kullanıcınınSectigiKoltuk>0 && koltuklar[kullanıcınınSectigiKoltuk-1]!=null)
+			{
+				Helper.EkraniTemizle();
+				Helper.EkranaYaz("Seçilen koltuk numarası: " + kullanıcınınSectigiKoltuk);
+				Helper.EkranaYaz(kullanıcınınSectigiKoltuk + " numaralı koltuğu daha önce " + koltuklar[kullanıcınınSectigiKoltuk - 1] + "isimli yolcuya rezerve ettiniz!");
+				Helper.EkranaYaz("Boş olan koltuklar: ");
+
+				for (int i = 1; i <=8; i++)
+				{
+					if (koltuklar[i]==null)
+					{
+						
+						Helper.EkranaYaz(koltuklar[i]);
+					}
+				}
+			}
+			else if(secim=="1" && kullanıcınınSectigiKoltuk < 9 && kullanıcınınSectigiKoltuk > 0 && koltuklar[kullanıcınınSectigiKoltuk - 1] == null)
+			{
+				Helper.EkraniTemizle();
+				Helper.EkranaYaz("Lütfen yolcunun adını ve soyadını yazınız.");
+				string yolcuBilgileri = Helper.EkrandanOku();
+				koltuklar[kullanıcınınSectigiKoltuk - 1]=yolcuBilgileri;
+				Helper.EkranaYaz(kullanıcınınSectigiKoltuk + " numaralı koltuk " + yolcuBilgileri + " adına ayrılmıştır. ");
+				Helper.EkranaYaz("İyi uçuşlar dileriz...");
+
+			}
+			//Economy
+			else if (secim == "2" && kullanıcınınSectigiKoltuk < 20 && kullanıcınınSectigiKoltuk > 8 && koltuklar[kullanıcınınSectigiKoltuk - 1] != null)
+			{
+				Helper.EkraniTemizle();
+				Helper.EkranaYaz("Seçilen koltuk numarası: " + kullanıcınınSectigiKoltuk);
+				Helper.EkranaYaz(kullanıcınınSectigiKoltuk + " numaralı koltuğu daha önce " + koltuklar[kullanıcınınSectigiKoltuk - 1] + " isimli yolcuya rezerve edilmiştir!");
+
+				for (int i = 9; i <21 ; i++)
+				{
+					if (koltuklar[i] == null)
+					{
+						Helper.EkranaYaz(i + 1);
+					}
+				}
+			}
+			
+			else if (secim == "2" && kullanıcınınSectigiKoltuk < 21 && kullanıcınınSectigiKoltuk > 8 && koltuklar[kullanıcınınSectigiKoltuk - 1] == null)
+			{
+				Helper.EkraniTemizle();
+				Helper.EkranaYaz(" Lütfen yolcunun adını ve soyadını yazınız. ");
+				string yolcuBilgileri = Helper.EkrandanOku();
+				koltuklar[kullanıcınınSectigiKoltuk - 1] = yolcuBilgileri;
+				Helper.EkranaYaz(kullanıcınınSectigiKoltuk + " numaralı koltuk " + yolcuBilgileri + " adına ayrılmıştır. ");
+				Helper.EkranaYaz("İyi uçuşlar dileriz...");
+
+			}
+
+			else
+			{
+				Helper.EkranaYaz("Bütün koltuklarımız doludur. Bir sonraki uçak 4 saat sonradır. ");
+			}
+		}
+		
+	}
+}
